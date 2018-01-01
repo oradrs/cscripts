6e3b46c237fd018d121ce1d48500485404bdb202
Darshak <oradrs@yahoo,com>
2018-01-01 14:05:40 +0530

    Generates HTML sqlMon report also
    Generates HTML sqlMon report also

--- sql/sqlmon.sql
+++ sql/sqlmon.sql
@@ -4,21 +4,11 @@
 --
 -- Purpose:     SQL Monitor Reports for one SQL_ID
 --
--- Author:      Carlos Sierra
---
--- Version:     2014/03/11
---
--- Usage:       This script inputs two parameters. Parameter 1 is a flag to specify if
---              your database is licensed to use the Oracle Tuning Pack or not.
---              Parameter 2 specifies the SQL_ID for which you want to generate all
---              SQL Monitor reports from all RAC nodes.
---              If you don't have the Oracle Tuning Pack license do not use this script.
 --
 -- Example:     @sqlmon.sql Y f995z9antmhxn
 --
 --  Notes:      Developed and tested on 11.2.0.3 and 12.0.1.0
---              Generates both TEXT and ACTIVE reports
---              For a more robust tool use SQLHC or SQLTXPLAIN(SQLT) from MOS
+--              Generates TEXT, HTML and ACTIVE reports
 --             
 ---------------------------------------------------------------------------------------
 -- text and or active formats? change these flags if you want to eliminate a format
@@ -103,6 +93,60 @@ SELECT DBMS_SQLTUNE.report_sql_monitor_list(sql_id => '&&sql_id.', type => 'TEXT
    AND '&&text.' = 'Y';
 @sqlmon_&&sql_id._driver.sql
 SPO OFF;
+-- ------------------------------------------
+--html
+SPO sqlmon_&&sql_id._driver.sql
+DECLARE
+  PROCEDURE put (p_line IN VARCHAR2)
+  IS
+  BEGIN
+    DBMS_OUTPUT.PUT_LINE(p_line);
+  END put;
+BEGIN
+  FOR i IN (SELECT * FROM 
+           (SELECT sid,
+                   session_serial#,
+                   sql_exec_start,
+                   sql_exec_id,
+                   inst_id
+              FROM gv$sql_monitor 
+             WHERE :license = 'Y' 
+               AND '&&active.' = 'Y'
+               AND sql_id = '&&sql_id.' 
+               AND sql_text IS NOT NULL
+               AND process_name = 'ora'
+             ORDER BY
+                   sql_exec_start DESC)
+             WHERE ROWNUM <= &&max_reports.)
+  LOOP
+    put('BEGIN');
+    put(':r :=');
+    put('DBMS_SQLTUNE.report_sql_monitor');
+    put('( sql_id => ''&&sql_id.''');
+    put(', session_id => '||i.sid);
+    put(', session_serial => '||i.session_serial#);
+    put(', sql_exec_start => TO_DATE('''||TO_CHAR(i.sql_exec_start, '&&date_mask.')||''', ''&&date_mask.'')');
+    put(', sql_exec_id => '||i.sql_exec_id);
+    put(', inst_id => '||i.inst_id);
+    put(', report_level => ''ALL''');
+    put(', type => ''HTML'' );');
+    put('END;');
+    put('/');
+    put('SPO sqlmon_&&sql_id._'||i.sql_exec_id||'_'||LPAD(TO_CHAR(i.sql_exec_start, 'HH24MISS'), 6, '0')||'_Html.html;');
+    put('PRINT :r;');
+    put('SPO OFF;');
+  END LOOP;
+END;
+/
+SPO OFF;
+SPO sqlmon_&&sql_id._list.html;
+SELECT DBMS_SQLTUNE.report_sql_monitor_list(sql_id => '&&sql_id.', type => 'HTML') 
+  FROM DUAL 
+ WHERE :license = 'Y' 
+   AND '&&active.' = 'Y';
+SPO OFF;
+@sqlmon_&&sql_id._driver.sql
+-- ------------------------------------------
 -- active
 SPO sqlmon_&&sql_id._driver.sql
 DECLARE
@@ -170,6 +214,5 @@ SET FEED ON VER ON LIN 80 PAGES 14 LONG 80 LONGC 80 TRIMS OFF;
 SET SERVEROUT OFF;
 UNDEF 1 2
 -- end
-
 
 
