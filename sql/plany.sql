8a3b8dcbb0db9bfbdd5a0467c27835a19d6394a4
Darshak <a@a.com>
2018-01-18 18:23:26 +0530

    added list of required scripts in this wrapper script

--- sql/plany.sql
+++ sql/plany.sql
@@ -3,5 +3,11 @@ DEF sql_id_y = '&1.'
 @planx.sql Y &&sql_id_y.
 @sqlperf.sql &&sql_id_y.
 --@dba_hist_sqlstat_sql_id_chart.sql &&sql_id_y. ""
+@dba_hist_sqlstat_dbtime_chart.sql "" N &&sql_id_y. ""
+@dba_hist_sqlstat_execs_chart.sql "" N &&sql_id_y. ""
+@dba_hist_sqlstat_bg_chart.sql "" N &&sql_id_y. ""
+@sqlash.sql Y &&sql_id_y.
+@sqlmon.sql Y &&sql_id_y.
+
 UNDEF sql_id_y
 UNDEF 1 2
